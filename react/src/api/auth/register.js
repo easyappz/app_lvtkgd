c96c00d6e205ef957b7f4b85c2ff12b82b47b159
IbragimKhaniev <newinterny@gmail.com>
2025-12-01 02:26:02 +0300

    fix(frontend): add trailing slash to /api/auth/register/ to resolve 404

--- react/src/api/auth/register.js
+++ react/src/api/auth/register.js
@@ -1,5 +1,5 @@
 import { instance } from '../axios.js';
 
 export const register = (data) => {
-  return instance.post('/api/auth/register', data);
+  return instance.post('/api/auth/register/', data);
 };
